fc1d0fbfffc5a0fda27e64636c554a1ab3c787ec
Isaac Sanders <isanders@drwholdings.com>
2020-10-20 09:01:18 -0500

    fix: Converts more ints to strings

--- models.go
+++ models.go
@@ -61,7 +61,7 @@ type Version struct {
 	PR                  string    `json:"pr"`
 	Commit              string    `json:"commit"`
 	CommittedDate       time.Time `json:"committed,omitempty"`
-	ApprovedReviewCount int       `json:"approved_review_count"`
+	ApprovedReviewCount string    `json:"approved_review_count"`
 }
 
 // NewVersion constructs a new Version.
